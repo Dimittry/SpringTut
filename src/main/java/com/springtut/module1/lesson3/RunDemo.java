b16695e3f1576a3ee448869d2f1c4f74a46b0686
olyver <demar.07@gmail.com>
2016-11-25 21:31:47 +0900

    Has been added example of factory beans.

--- src/main/java/com/springtut/module1/lesson3/RunDemo.java
+++ src/main/java/com/springtut/module1/lesson3/RunDemo.java
@@ -5,6 +5,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.text.NumberFormat;
+
 public class RunDemo {
     public static void main(String[] args) {
 //        ApplicationContext context =
@@ -13,6 +15,10 @@ public class RunDemo {
                 new ClassPathXmlApplicationContext("applicationContext.xml");
         Game game = context.getBean("game", Game.class);
         System.out.println(game.playGame());
+
+        NumberFormat nf = context.getBean(NumberFormat.class);
+        double amount = 12345678.9012345;
+        System.out.println(nf.format(amount));
 
         System.out.println("There are " + context.getBeanDefinitionCount() + " beans");
         for(String name : context.getBeanDefinitionNames()) {
