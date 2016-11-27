69307048b6e8a6c35221cb1b98e85d2ba9f1c9d5
olyver <demar.07@gmail.com>
2016-11-28 01:35:23 +0900

    Has been added EmbeddedDatabaseBuilder.

--- src/main/java/com/springtut/module3/RunDemo.java
+++ src/main/java/com/springtut/module3/RunDemo.java
@@ -1,7 +1,17 @@
 package com.springtut.module3;
 
-/**
- * Created by Dimon on 26.11.2016.
- */
+import com.springtut.module3.config.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class RunDemo {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(
+                AppConfig.class, AppConfig.class
+        );
+        System.out.println(context.getBeanDefinitionCount());
+        for(String name : context.getBeanDefinitionNames()) {
+            System.out.println(name);
+        }
+    }
 }
--- src/main/java/com/springtut/module3/config/AppConfig.java
+++ src/main/java/com/springtut/module3/config/AppConfig.java
@@ -1,7 +1,59 @@
 package com.springtut.module3.config;
 
-/**
- * Created by Dimon on 26.11.2016.
- */
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan(basePackages = "com.springtut.module3")
+@PropertySource("classpath:prod.properties")
+@EnableTransactionManagement
 public class AppConfig {
+    @Autowired
+    private Environment env;
+
+    @Bean(name = "dataSource")
+    @Profile("test")
+    public DataSource dataSourceForTest() {
+        return new EmbeddedDatabaseBuilder()
+                .generateUniqueName(true)
+                .setType(EmbeddedDatabaseType.H2)
+                .setScriptEncoding("UTF-8")
+                .ignoreFailedDrops(true)
+                .addScript("schema.sql")
+                .addScripts("data.sql")
+                .build();
+    }
+
+    @Bean(name = "transactionManager")
+    @Profile("test")
+    public PlatformTransactionManager transactionManagerForTest() {
+        return new DataSourceTransactionManager(dataSourceForTest());
+    }
+
+    @Bean(name = "dataSource")
+    @Profile({"prod", "test"})
+    public DataSource dataSourceForProd() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(env.getProperty("db.driver"));
+        dataSource.setUrl(env.getProperty("db.url"));
+        dataSource.setUsername(env.getProperty("db.user"));
+        dataSource.setPassword(env.getProperty("db.pass"));
+        return dataSource;
+    }
+
+    @Bean(name = "transactionManager")
+    @Profile({"prod", "test"})
+    public PlatformTransactionManager transactionManagerForProd() {
+        return new DataSourceTransactionManager(dataSourceForProd());
+    }
+
 }
